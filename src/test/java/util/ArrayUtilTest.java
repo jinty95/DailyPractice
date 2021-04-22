09a9dc5d624f54724ccf1c3015f90b176dd40438
wangjintai <wangjintai@aspirecn.com>
2021-04-22 16:32:57 +0800

    数组工具：键盘输入数组

---
+++ src/test/java/util/ArrayUtilTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import cn.jinty.utils.ArrayUtil;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 数组工具类 - 测试
+ *
+ * @author jinty
+ * @date 2021/4/22
+ **/
+public class ArrayUtilTest {
+
+    @Test
+    public void testGenerateArray(){
+        int[] arr = ArrayUtil.generateArray(10);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testInputArray(){
+        int[] arr = ArrayUtil.inputArray();
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
