ed5f596886fb96db4e5bede20e0f44b49a30111c
wangjintai <1812154599@qq.com>
2021-06-02 20:27:12 +0800

    工具类：狗屁不通生成器

---
+++ src/test/java/util/BoshUtilTest.java
@@ -0,0 +1,23 @@
+package util;
+
+import cn.jinty.util.BoshUtil;
+import org.junit.Test;
+
+/**
+ * 狗屁不通生成器 - 测试
+ *
+ * @author jinty
+ * @date 2021/6/2
+ **/
+public class BoshUtilTest {
+
+    @Test
+    public void test(){
+        System.out.println(
+                BoshUtil.generate(
+                        "狗屁不通生成器",1600
+                )
+        );
+    }
+
+}
