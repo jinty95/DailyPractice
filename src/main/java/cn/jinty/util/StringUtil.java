bed680d574477d2e75418d6030308fd142c1873d
wangjintai <1812154599@qq.com>
2021-06-03 17:54:11 +0800

    字符串工具：驼峰命名与下划线命名转换

--- src/main/java/cn/jinty/util/StringUtil.java
+++ src/main/java/cn/jinty/util/StringUtil.java
@@ -1,7 +1,7 @@
 package cn.jinty.util;
 
 /**
- * 字符串工具类
+ * 字符串 - 工具类
  *
  * @author jinty
  * @date 2021/4/9
@@ -9,8 +9,10 @@ package cn.jinty.util;
 public final class StringUtil {
 
     //十六进制字符集
-    public static char[] hexChar =
-            {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    public static char[] hexChar = {
+            '0','1','2','3','4','5','6','7',
+            '8','9','A','B','C','D','E','F'
+    };
 
     /**
      * 字节数组转为十六进制字符串
@@ -23,12 +25,11 @@ public final class StringUtil {
         if(bytes==null || bytes.length==0) return null;
         //字符串拼接
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<bytes.length;i++){
-            byte b = bytes[i];
+        for (byte b : bytes) {
             //高4位
-            sb.append(hexChar[(b>>>4)&15]);
+            sb.append(hexChar[(b >>> 4) & 15]);
             //低4位
-            sb.append(hexChar[b&15]);
+            sb.append(hexChar[b & 15]);
         }
         return sb.toString();
     }
@@ -50,7 +51,54 @@ public final class StringUtil {
      * @return 是否空白
      */
     public static boolean isBlank(String s){
-        return s == null || s.length()==0 || s.trim().length()==0;
+        return isEmpty(s) || s.trim().length()==0;
+    }
+
+    /**
+     * 下划线命名转驼峰命名
+     *
+     * @param snake 下划线命名
+     * @return 驼峰命名
+     */
+    public static String snakeToCamel(String snake){
+        if(StringUtil.isBlank(snake)
+                || snake.charAt(0)=='_'
+                || snake.charAt(snake.length()-1)=='_'){
+            throw new IllegalArgumentException(String.format("illegal snake name [%s]",snake));
+        }
+        StringBuilder camel = new StringBuilder();
+        for(int i=0;i<snake.length();i++){
+            if(snake.charAt(i)!='_'){
+                camel.append(snake.charAt(i));
+            }else{
+                i++;
+                camel.append((char)(snake.charAt(i)-32));
+            }
+        }
+        return camel.toString();
+    }
+
+    /**
+     * 驼峰命名转下划线命名
+     *
+     * @param camel 驼峰命名
+     * @return 下划线命名
+     */
+    public static String camelToSnake(String camel){
+        if(StringUtil.isBlank(camel)
+                || (camel.charAt(0)>='A' && camel.charAt(0)<='Z')){
+            throw new IllegalArgumentException(String.format("illegal camel name [%s]",camel));
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<camel.length();i++){
+            if(camel.charAt(i)>='A' && camel.charAt(i)<='Z'){
+                sb.append('_');
+                sb.append((char)(camel.charAt(i)+32));
+            }else{
+                sb.append(camel.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
 }
---
+++ src/test/java/util/StringUtilTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import cn.jinty.util.StringUtil;
+import org.junit.Test;
+
+/**
+ * 字符串 - 工具类 - 测试
+ *
+ * @author jinty
+ * @date 2021/6/3
+ **/
+public class StringUtilTest {
+
+    @Test
+    public void testSnakeToCamel(){
+        System.out.println(StringUtil.snakeToCamel("hello_world_are_you_ok"));
+        System.out.println(StringUtil.snakeToCamel("my_name"));
+        System.out.println(StringUtil.snakeToCamel("_my_name_"));
+    }
+
+    @Test
+    public void testCamelToSnake(){
+        System.out.println(StringUtil.camelToSnake("helloWorldAreYouOk"));
+        System.out.println(StringUtil.camelToSnake("myName"));
+        System.out.println(StringUtil.camelToSnake("MyName"));
+    }
+
+}
