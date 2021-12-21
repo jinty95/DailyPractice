168ac812db13600390abd1b15750c2995c115de4
wangjintai <1812154599@qq.com>
2021-12-21 13:14:30 +0800

    gitignore：整理忽略信息

--- src/main/java/cn/jinty/Main.java
+++ src/main/java/cn/jinty/Main.java
@@ -10,7 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        System.out.println(System.getenv());
+        System.out.println(System.getenv("JAVA_HOME"));
 
     }
 
