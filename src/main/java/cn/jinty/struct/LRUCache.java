9bd8629a08c9603db24b04db53bdd14ba1a165e0
wangjintai <1812154599@qq.com>
2022-05-25 15:28:33 +0800

    数据结构：LRUCache

--- src/main/java/cn/jinty/struct/LRUCache.java
+++ src/main/java/cn/jinty/struct/LRUCache.java
@@ -2,42 +2,65 @@ package cn.jinty.struct;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.*;
 
 /**
- * Least Recently Used 最近使用时间最小
- * 是一种常用的淘汰策略，将上一次使用时间离现在最久的元素删除
+ * 缓存
+ * 存储形式：哈希表 (通过元素键快速获取元素值)
+ * 淘汰方式：LRU - Least Recently Used (将最近使用时间最小的元素删除)
+ * 过期实现：延时线程池 (在存入元素时建立一个延时任务，到期删除元素)
  *
  * @author Jinty
  * @date 2021/3/15
  **/
-public class LRUCache extends LinkedHashMap<Integer, Integer> {
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
 
+    // 最大容量
     private final int capacity;
 
-    //初始化
+    // 延时线程池
+    private final ScheduledExecutorService executorService;
+
+    /**
+     * 初始化
+     *
+     * @param capacity 最大容量
+     */
     public LRUCache(int capacity) {
         super(capacity, 0.75f, true);
         this.capacity = capacity;
-    }
-
-    //查询
-    public int get(int key) {
-        return super.get(key) == null ? -1 : super.get(key);
-    }
-
-    //存入
-    public void put(int key, int value) {
-        super.put(key, value);
+        this.executorService = new ScheduledThreadPoolExecutor(8);
     }
 
     @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+    public boolean removeEldestEntry(Map.Entry<K, V> eldest) {
         return super.size() > capacity;
     }
 
-    @Override
-    public String toString() {
-        return super.toString();
+    /**
+     * 存入键值对，并设置过期时间
+     *
+     * @param key        键
+     * @param value      值
+     * @param expireTime 过期时间(毫秒)
+     * @return 值
+     */
+    public V put(K key, V value, long expireTime) {
+        value = super.put(key, value);
+        if (expireTime > 0) {
+            removeExpireEntry(key, expireTime);
+        }
+        return value;
+    }
+
+    /**
+     * 移除过期元素
+     *
+     * @param key        元素键
+     * @param expireTime 过期时间(毫秒)
+     */
+    private void removeExpireEntry(Object key, Long expireTime) {
+        executorService.schedule(() -> super.remove(key), expireTime, TimeUnit.MILLISECONDS);
     }
 
 }
--- src/test/java/test/cn/jinty/struct/LRUCacheTest.java
+++ src/test/java/test/cn/jinty/struct/LRUCacheTest.java
@@ -13,17 +13,27 @@ public class LRUCacheTest {
 
     @Test
     public void test() {
-        LRUCache lruCache = new LRUCache(3);
-        lruCache.put(1, 1);
-        lruCache.put(2, 2);
-        lruCache.put(3, 3);
-        System.out.println(lruCache); // 1,2,3
-        lruCache.get(2);
-        System.out.println(lruCache); // 1,3,2
-        lruCache.get(1);
-        System.out.println(lruCache); // 3,2,1
-        lruCache.put(4, 4);
-        System.out.println(lruCache); // 2,1,4
+        try {
+            LRUCache<Integer, Integer> lruCache = new LRUCache<>(3);
+            lruCache.put(1, 1);
+            lruCache.put(2, 2);
+            lruCache.put(3, 3);
+            System.out.println(lruCache); // 1,2,3
+            lruCache.get(2);
+            System.out.println(lruCache); // 1,3,2
+            lruCache.get(1);
+            System.out.println(lruCache); // 3,2,1
+            lruCache.put(4, 4, 1000L);
+            System.out.println(lruCache); // 2,1,4
+            Thread.sleep(2000L);
+            System.out.println(lruCache); // 2,1
+            lruCache.put(5, 5, -1L);
+            System.out.println(lruCache); // 2,1,5
+            Thread.sleep(2000L);
+            System.out.println(lruCache); // 2,1,5
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
