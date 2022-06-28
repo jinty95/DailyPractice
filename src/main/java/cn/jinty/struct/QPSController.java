a7b3991a5b2046657483c1475edf90160cfad896
wangjintai <1812154599@qq.com>
2022-06-28 15:33:16 +0800

    数据结构：QPS控制器

---
+++ src/main/java/cn/jinty/struct/QPSController.java
@@ -0,0 +1,61 @@
+package cn.jinty.struct;
+
+/**
+ * QPS控制器
+ * 使用数组存储每个请求到来的时间，前 N 次请求顺利通过，并填满数组。
+ * 后续请求到来时，判断当前时间是否比数组中最早的时间晚 1 s，是则通过，否则拒绝。
+ *
+ * @author jintai.wang
+ * @date 2022/6/28
+ **/
+public class QPSController {
+
+    // 单位时间请求限制次数
+    private final int limit;
+
+    // 时间单位(毫秒表示)
+    private final long time;
+
+    // 存储请求时间的容器
+    private final long[] container;
+
+    // 指针，标识容器中的下一个可用位置
+    private int position = 0;
+
+    /**
+     * 构造器
+     *
+     * @param limit 单位时间请求限制次数
+     * @param time  时间单位(毫秒表示)
+     */
+    public QPSController(int limit, long time) {
+        this.limit = limit;
+        this.time = time;
+        this.container = new long[limit];
+    }
+
+    /**
+     * 请求占用容器的下一个位置
+     *
+     * @return 是否占用成功
+     */
+    public synchronized boolean next() {
+        long cur = System.currentTimeMillis();
+        if (cur >= container[position] + time) {
+            container[position] = cur;
+            position = (position + 1) % limit;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 当占位失败时，计算最短等待时间
+     *
+     * @return 等待时间(毫秒表示)
+     */
+    public long waitTime() {
+        return container[position] + time - System.currentTimeMillis();
+    }
+
+}
---
+++ src/test/java/test/cn/jinty/struct/QPSControllerTest.java
@@ -0,0 +1,59 @@
+package test.cn.jinty.struct;
+
+import cn.jinty.struct.QPSController;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * QPS控制器 - 测试
+ *
+ * @author jintai.wang
+ * @date 2022/6/28
+ **/
+public class QPSControllerTest {
+
+    @Test
+    public void test() {
+        QPSController qpsController = new QPSController(100, TimeUnit.SECONDS.toMillis(1));
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i <= 1000; i++) {
+            final int j = i;
+            Thread t = new Thread(() -> {
+                if (qpsController.next()) {
+                    System.out.printf("请求%s正常执行\n", j);
+                } else {
+                    long totalWaitTime = 0L;
+                    while (true) {
+                        try {
+                            long waitTime = qpsController.waitTime();
+                            if (waitTime > 0) {
+                                totalWaitTime += waitTime;
+                                Thread.sleep(waitTime);
+                            }
+                            if (qpsController.next()) {
+                                System.out.printf("请求%s延迟%s毫秒后正常执行\n", j, totalWaitTime);
+                                break;
+                            }
+                        } catch (Exception e) {
+                            System.out.printf("请求%s执行异常：%s\n", j, e.getMessage());
+                            break;
+                        }
+                    }
+                }
+            });
+            threads.add(t);
+            t.start();
+        }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
