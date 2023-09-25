9836921ba9f084958ec95bda34a2a45784b0a447
wangjintai <1812154599@qq.com>
2023-09-25 22:41:27 +0800

    摘要工具：计算文件摘要

--- src/main/java/cn/jinty/util/security/MessageDigestUtil.java
+++ src/main/java/cn/jinty/util/security/MessageDigestUtil.java
@@ -2,7 +2,12 @@ package cn.jinty.util.security;
 
 import cn.jinty.util.string.HexStringUtil;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
+import java.util.Arrays;
 
 /**
  * 信息摘要 - 工具类
@@ -15,79 +20,134 @@ public final class MessageDigestUtil {
     private MessageDigestUtil() {
     }
 
-    public static final String MD5 = "MD5";
-    public static final String SHA1 = "SHA1";
-
-    /**
-     * 生成MD5摘要
-     *
-     * @param bytes 原内容(字节流)
-     * @return 摘要(字节流)
-     * @throws Exception 异常
-     */
-    public static byte[] md5(byte[] bytes) throws Exception {
-        MessageDigest mdInst = MessageDigest.getInstance(MD5);
-        mdInst.update(bytes);
-        return mdInst.digest();
-    }
-
-    /**
-     * 生成MD5摘要
-     *
-     * @param s 原内容(字符串)
-     * @return 摘要(字节流)
-     * @throws Exception 异常
-     */
-    public static byte[] md5(String s) throws Exception {
-        return s == null ? new byte[0] : md5(s.getBytes());
+    // 摘要算法
+    public enum ALGORITHM {
+        MD5, SHA1
     }
 
     /**
      * 生成MD5摘要字符串
      *
-     * @param s 原内容(字符串)
+     * @param s 内容(字符串)
      * @return 摘要(十六进制字符串)
-     * @throws Exception 异常
      */
-    public static String md5Str(String s) throws Exception {
+    public static String md5Str(String s) {
         // 字节流转换成十六进制的字符串形式
-        return HexStringUtil.bytesToHex(md5(s), false);
+        return HexStringUtil.bytesToHex(md(s.getBytes(), ALGORITHM.MD5), false);
     }
 
     /**
-     * 生成SHA1摘要
+     * 生成MD5摘要字符串
      *
-     * @param bytes 原内容(字节流)
-     * @return 摘要(字节流)
-     * @throws Exception 异常
+     * @param is 内容(输入流)
+     * @return 摘要(十六进制字符串)
      */
-    public static byte[] sha1(byte[] bytes) throws Exception {
-        MessageDigest mdInst = MessageDigest.getInstance(SHA1);
-        mdInst.update(bytes);
-        return mdInst.digest();
+    public static String md5Str(InputStream is) {
+        // 字节流转换成十六进制的字符串形式
+        return HexStringUtil.bytesToHex(md(is, ALGORITHM.MD5), false);
     }
 
     /**
-     * 生成SHA1摘要
+     * 生成MD5摘要字符串
      *
-     * @param s 原内容(字符串)
-     * @return 摘要(字节流)
-     * @throws Exception 异常
+     * @param file 内容(文件)
+     * @return 摘要(十六进制字符串)
      */
-    public static byte[] sha1(String s) throws Exception {
-        return s == null ? new byte[0] : sha1(s.getBytes());
+    public static String md5Str(File file) {
+        // 字节流转换成十六进制的字符串形式
+        return HexStringUtil.bytesToHex(md(file, ALGORITHM.MD5), false);
     }
 
     /**
      * 生成SHA1摘要字符串
      *
-     * @param s 原内容(字符串)
+     * @param s 内容(字符串)
      * @return 摘要(十六进制字符串)
-     * @throws Exception 异常
      */
-    public static String sha1Str(String s) throws Exception {
+    public static String sha1Str(String s) {
         // 字节流转换成十六进制的字符串形式
-        return HexStringUtil.bytesToHex(sha1(s), false);
+        return HexStringUtil.bytesToHex(md(s.getBytes(), ALGORITHM.SHA1), false);
+    }
+
+    /**
+     * 生成SHA1摘要字符串
+     *
+     * @param is 内容(输入流)
+     * @return 摘要(十六进制字符串)
+     */
+    public static String sha1Str(InputStream is) {
+        // 字节流转换成十六进制的字符串形式
+        return HexStringUtil.bytesToHex(md(is, ALGORITHM.SHA1), false);
+    }
+
+    /**
+     * 生成SHA1摘要字符串
+     *
+     * @param file 内容(文件)
+     * @return 摘要(十六进制字符串)
+     */
+    public static String sha1Str(File file) {
+        // 字节流转换成十六进制的字符串形式
+        return HexStringUtil.bytesToHex(md(file, ALGORITHM.SHA1), false);
+    }
+
+    /* 以下为内部函数 */
+
+    /**
+     * 生成摘要
+     *
+     * @param bytes     内容(字节流)
+     * @param algorithm 摘要算法
+     * @return 摘要(字节流)
+     */
+    private static byte[] md(byte[] bytes, ALGORITHM algorithm) {
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance(algorithm.name());
+            mdInst.update(bytes);
+            return mdInst.digest();
+        } catch (Exception e) {
+            System.out.printf("生成摘要异常：bytes=%s, algorithm=%s%n", Arrays.toString(bytes), algorithm);
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 生成摘要
+     *
+     * @param is        内容(输入流)
+     * @param algorithm 摘要算法
+     * @return 摘要(字节流)
+     */
+    private static byte[] md(InputStream is, ALGORITHM algorithm) {
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance(algorithm.name());
+            DigestInputStream dis = new DigestInputStream(is, mdInst);
+            byte[] buf = new byte[1024];
+            while (dis.read(buf) != -1) ;
+            return dis.getMessageDigest().digest();
+        } catch (Exception e) {
+            System.out.printf("生成摘要异常：algorithm=%s%n", algorithm);
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 生成摘要
+     *
+     * @param file      内容(文件)
+     * @param algorithm 摘要算法
+     * @return 摘要(字节流)
+     */
+    private static byte[] md(File file, ALGORITHM algorithm) {
+        try {
+            return md(new FileInputStream(file), algorithm);
+        } catch (Exception e) {
+            System.out.printf("生成摘要异常：file=%s, algorithm=%s%n", file.getAbsolutePath(), algorithm);
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
--- src/test/java/test/cn/jinty/util/security/MessageDigestUtilTest.java
+++ src/test/java/test/cn/jinty/util/security/MessageDigestUtilTest.java
@@ -3,6 +3,8 @@ package test.cn.jinty.util.security;
 import cn.jinty.util.security.MessageDigestUtil;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * 信息摘要 - 工具类 - 测试
  *
@@ -23,6 +25,16 @@ public class MessageDigestUtilTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void test1() {
+        File file = new File("C:\\Users\\wangjintai\\Documents\\TombRaider.log");
+        String md5 = MessageDigestUtil.md5Str(file);
+        String sha1 = MessageDigestUtil.sha1Str(file);
+        System.out.println(file.getAbsolutePath());
+        System.out.println("MD5：" + md5 + ", length=" + md5.length());
+        System.out.println("SHA1：" + sha1 + ", length=" + sha1.length());
     }
 
 }
